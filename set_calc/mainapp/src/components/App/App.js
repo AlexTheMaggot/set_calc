cb6eb318484520dba0cc4bb6178a966341f93686
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-10 18:04:14 +0500

    - add App component

---
+++ set_calc/mainapp/src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import {createRoot} from "react-dom/client";
+import './App.css'
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="app">
+            </div>
+        );
+    }
+}
+
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
