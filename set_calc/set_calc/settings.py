f08fd5cbe8e28169d23ddcc4b81e3f4efebfa41c
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-12 16:23:25 +0500

    - setup static and media

--- set_calc/set_calc/settings.py
+++ set_calc/set_calc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from .config import *
 
@@ -117,8 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'mainapp/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mainapp/media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
