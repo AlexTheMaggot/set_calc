7a73832e206ed3913f2a6766c13854406379c380
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-14 15:04:37 +0500

    - setup preloader timing

--- set_calc/mainapp/static/js/script.js
+++ set_calc/mainapp/static/js/script.js
@@ -1,9 +1,7 @@
 window.addEventListener('load', () => {
         let preloader = document.getElementById('preloader')
-        setTimeout(() => {
-                preloader.classList.add('preloader_transparent')
-        }, 2000)
+        preloader.classList.add('preloader_transparent')
         setTimeout(() => {
                 preloader.classList.add('preloader_none')
-        }, 2300)
+        }, 300)
 })
