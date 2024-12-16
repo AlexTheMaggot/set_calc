1e1460774e707f22d23f87a9ce702975b6c918ff
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-16 16:32:54 +0500

    - setup api reciver ad connect api methods

--- set_calc/api/views.py
+++ set_calc/api/views.py
@@ -1,11 +1,23 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponseForbidden, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from .api_methods import *
+
+methods = {
+    'AuthCheck': auth_check,
+}
 
 
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        return JsonResponse({'result': 'Success'})
+        request_data = json.loads(request.body)
+        method = request_data['method']
+        if method in methods.keys():
+            run = methods[request_data['method']]
+            return run(request, request_data)
+        else:
+            return make_error(request_data['id'], 1002)
     else:
         return HttpResponseForbidden()
