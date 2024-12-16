794fb894a81cad16c8a62c79df41c531c7817b3b
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-16 16:35:25 +0500

    - add home and auth routes

--- set_calc/mainapp/src/components/App/App.js
+++ set_calc/mainapp/src/components/App/App.js
@@ -1,15 +1,22 @@
-import React, {Component} from "react";
+import React from "react";
 import {createRoot} from "react-dom/client";
-import './App.css'
-
-
+import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Main from "../Main/Main";
 export default function App(props) {
     return (
-        <div className="app"></div>
-    )
+        <div className="app">
+            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+                <Routes>
+                    <Route exact path="/" element={<Main level="home"/>}></Route>
+                    <Route exact path="/ru/" element={<Main level="home_ru"/>}></Route>
+                    <Route exact path="/uz/" element={<Main level="home_uz"/>}></Route>
+                    <Route exact path="/auth/" element={<Main level="auth"/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
-
-
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<App/>);
