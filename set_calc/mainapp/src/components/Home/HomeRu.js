e8bdf6d658a787591aae8d7042de86b6709679bf
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-26 15:36:40 +0500

    - remove home component

--- set_calc/mainapp/src/components/Home/HomeRu.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function HomeRu(props) {
-    return(
-        <div className="home">
-            <h1>Home Ru</h1>
-        </div>
-    )
-}
--- set_calc/mainapp/src/components/Home/HomeUz.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function HomeUz(props) {
-    return(
-        <div className="home">
-            <h1>Home Uz</h1>
-        </div>
-    )
-}
