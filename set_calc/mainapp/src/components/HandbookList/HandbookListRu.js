b8dd097f5d9abd00cbb01a1be075f3ef24cec045
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-26 15:47:11 +0500

    - add handbook list component

---
+++ set_calc/mainapp/src/components/HandbookList/HandbookListRu.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HandbookListRu.css"
+export default function HandbookListRu(props) {
+    return (
+        <div className="handbooklist">
+            <h1>Handbook List Ru</h1>
+        </div>
+    )
+}
---
+++ set_calc/mainapp/src/components/HandbookList/HandbookListUz.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HandbookListUz.css";
+export default function HandbookListUz(props) {
+    return (
+        <div className="handbooklist">
+            <h1>Handbook List Uz</h1>
+        </div>
+    )
+}
