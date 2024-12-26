b622265afdd77ef6d76a4796ba4cd83a31e13df3
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-26 15:44:51 +0500

    - remove home urls and add calculations coefficients and handbooks urls

--- set_calc/mainapp/urls.py
+++ set_calc/mainapp/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('ru/', views.index),
-    path('uz/', views.index),
+    path('ru/calculations/', views.index),
+    path('uz/calculations/', views.index),
+    path('ru/coefficients/', views.index),
+    path('uz/coefficients/', views.index),
+    path('ru/handbooks/', views.index),
+    path('uz/handbooks/', views.index),
     path('auth/', views.index),
 ]
