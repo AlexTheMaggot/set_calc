8dd1d33802b79d50236707898caab3f66577e3b2
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-26 15:35:56 +0500

    - remove home routes and add calculation coefficient and handbook routes

--- set_calc/mainapp/src/components/App/App.js
+++ set_calc/mainapp/src/components/App/App.js
@@ -9,8 +9,12 @@ export default function App(props) {
             <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
                 <Routes>
                     <Route exact path="/" element={<Main level="home"/>}></Route>
-                    <Route exact path="/ru/" element={<Main level="home_ru"/>}></Route>
-                    <Route exact path="/uz/" element={<Main level="home_uz"/>}></Route>
+                    <Route exact path="/ru/calculations/" element={<Main level="calculation_list_ru"/>}></Route>
+                    <Route exact path="/ru/coefficients/" element={<Main level="coefficient_list_ru"/>}></Route>
+                    <Route exact path="/ru/handbooks/" element={<Main level="handbook_list_ru"/>}></Route>
+                    <Route exact path="/uz/calculations/" element={<Main level="calculation_list_uz"/>}></Route>
+                    <Route exact path="/uz/coefficients/" element={<Main level="coefficient_list_uz"/>}></Route>
+                    <Route exact path="/uz/handbooks/" element={<Main level="handbook_list_uz"/>}></Route>
                     <Route exact path="/auth/" element={<Main level="auth"/>}></Route>
                 </Routes>
             </BrowserRouter>
