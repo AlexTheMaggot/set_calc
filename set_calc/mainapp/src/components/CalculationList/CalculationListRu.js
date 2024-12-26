d7b8d1176950eeebcfc8b9f399ea8b22729af1ee
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-26 15:46:29 +0500

    - add calculation list component

---
+++ set_calc/mainapp/src/components/CalculationList/CalculationListRu.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CalculationListRu.css";
+export default function CalculationListRu(props) {
+    return (
+        <div className="calculationlist">
+            <h1>Calculation List Ru</h1>
+        </div>
+    )
+}
---
+++ set_calc/mainapp/src/components/CalculationList/CalculationListUz.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CalculationListUz.css";
+export default function CalculationListUz(props) {
+    return (
+        <div className="calculationlist">
+            <h1>Calculation List Uz</h1>
+        </div>
+    )
+}
