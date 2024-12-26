e2c63a1909fd2981c8041ed9b52e6776c6d83b1d
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-26 15:46:52 +0500

    - add coefficient list component

---
+++ set_calc/mainapp/src/components/CoefficientList/CoefficientListRu.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CoefficientListRu.css"
+export default function CoefficientListRu(props) {
+    return (
+        <div className="coefficientlist">
+            <h1>Coefficient List Ru</h1>
+        </div>
+    )
+}
---
+++ set_calc/mainapp/src/components/CoefficientList/CoefficientListUz.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CoefficientListUz.css";
+export default function CoefficientListUz(props) {
+    return (
+        <div className="coefficientlist">
+            <h1>Coefficient List Uz</h1>
+        </div>
+    )
+}
