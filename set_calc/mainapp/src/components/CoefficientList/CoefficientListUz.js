dcc4112c810e6780f6c65f36d5c5a60626d0f72c
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-01-19 23:20:04 +0500

    - delete uz version

--- set_calc/mainapp/src/components/CoefficientList/CoefficientListUz.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./CoefficientListUz.css";
-export default function CoefficientListUz(props) {
-    return (
-        <div className="coefficientlist">
-            <h1>Coefficient List Uz</h1>
-        </div>
-    )
-}
