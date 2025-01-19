84e1c68b596d66340430186687f6c67f8c594cad
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-01-20 01:02:55 +0500

    - add nav links

--- set_calc/mainapp/src/components/Sidebar/Sidebar.js
+++ set_calc/mainapp/src/components/Sidebar/Sidebar.js
@@ -5,8 +5,26 @@ export default function Sidebar(props) {
     const text_content = {
         "main_ru": "Sidebar Ru",
         "main_uz": "Sidebar Uz",
+        "calculations_ru": "Рассчеты",
+        "calculations_uz": "Hisob-kitoblar",
+        "handbooks_ru": "Справочники",
+        "handbooks_uz": "Kataloglar",
+        "coefficients_ru": "Коэффициенты",
+        "coefficients_uz": "Koeffitsientlar",
     }
     return (
-        <h1>{props.lang === "ru" ? text_content.main_ru : text_content.main_uz}</h1>
+        <div className="sidebar">
+            <ul className="sidebar__ul">
+                <li className="sidebar__li">
+                    <a href="#" onClick={(e) => {props.navi(e, 'calculation_list')}} className="sidebar__link">{text_content["calculations_" + props.lang]}</a>
+                </li>
+                <li className="sidebar__li">
+                    <a href="#" onClick={(e) => {props.navi(e, 'coefficient_list')}} className="sidebar__link">{text_content["coefficients_" + props.lang]}</a>
+                </li>
+                <li className="sidebar__li">
+                    <a href="#" onClick={(e) => {props.navi(e, 'handbook_list')}} className="sidebar__link">{text_content["handbooks_" + props.lang]}</a>
+                </li>
+            </ul>
+        </div>
     )
 }
