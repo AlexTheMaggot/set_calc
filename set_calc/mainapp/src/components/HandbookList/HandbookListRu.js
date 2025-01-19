152ced377f0fef81777ed157deed9cc3db00b71e
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-01-19 23:22:08 +0500

    - remove ru version

--- set_calc/mainapp/src/components/HandbookList/HandbookListRu.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./HandbookListRu.css"
-export default function HandbookListRu(props) {
-    return (
-        <div className="handbooklist">
-            <h1>Handbook List Ru</h1>
-        </div>
-    )
-}
