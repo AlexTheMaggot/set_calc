62557911219c5a43ecb44faa56bc5041ea8a6954
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-01-21 02:12:06 +0500

    - add basic interface

--- set_calc/mainapp/src/components/CalculationList/CalculationList.js
+++ set_calc/mainapp/src/components/CalculationList/CalculationList.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./CalculationList.css";
 export default function CalculationList(props) {
     const text_content = {
-        "main_ru": "Calculation List Ru",
-        "main_uz": "Calculation List Uz",
+        "main_ru": "Мои Рассчеты",
+        "main_uz": "Mening hisoblarim",
+        "add_ru": "Новый рассчет",
+        "add_uz": "Yangi hisoblash",
     }
     return (
         <div className="calculationlist">
-            <h1>{props.lang === "ru" ? text_content.main_ru : text_content.main_uz}</h1>
+            <div className="calculationlist__block">
+                <div className="calculationlist__top">
+                    <h1 className="calculationlist__title">{props.lang === "ru" ? text_content.main_ru : text_content.main_uz}</h1>
+                    <button className="calculationlist__add-calculation">{text_content["add_" + props.lang]}</button>
+                </div>
+            </div>
         </div>
     )
 }
