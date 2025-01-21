730ba5a2c6c0f4307e197b1d0e009c020bec6f51
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-01-21 12:15:51 +0500

    - add basic interface

--- set_calc/mainapp/src/components/HandbookList/HandbookList.js
+++ set_calc/mainapp/src/components/HandbookList/HandbookList.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./HandbookList.css"
 export default function HandbookList(props) {
     const text_content = {
-        "main_ru": "Handbook List Ru",
-        "main_uz": "Handbook List Uz",
+        "main_ru": "Справочники",
+        "main_uz": "Kataloglar",
     }
     return (
         <div className="handbooklist">
-            <h1>{props.lang === "ru" ? text_content.main_ru : text_content.main_uz}</h1>
+            <div className="handbooklist__block">
+                <div className="handbooklist__top">
+                    <h1 className="handbooklist__title">{text_content["main_" + props.lang]}</h1>
+                </div>
+            </div>
         </div>
     )
 }
