28cdf67017743a39df8630f7dd40e3c311cbc07c
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-03-04 16:35:25 +0500

    - optimize calculation modal closing

--- set_calc/mainapp/src/components/NewCalculation/NewCalculation.js
+++ set_calc/mainapp/src/components/NewCalculation/NewCalculation.js
@@ -12,12 +12,12 @@ export default function NewCalculation(props) {
         let r = api_sender('CalculationAdd', 1)
         r.then(data => {
             props.set_calculations_update(true)
-            props.close_new_calculation()
+            props.close_calculation_modal(props.set_new_calculation_modal)
         })
     }
     return (
-        <div onClick={props.close_new_calculation} className={props.new_calculation_appear ? "new-calculation" : "new-calculation new-calculation_hidden"}>
-            <div onClick={(e) => {e.stopPropagation()}} className={"new-calculation__block" + (props.new_calculation_block_appear !== true ? " new-calculation__block_hidden" : "") + (props.new_calculation_block_show !== true ? " new-calculation__block_none" : "")}>
+        <div onClick={() => {props.close_calculation_modal(props.set_new_calculation_modal)}} className={props.new_calculation_modal.wrapper.appear ? "new-calculation" : "new-calculation new-calculation_hidden"}>
+            <div onClick={(e) => {e.stopPropagation()}} className={"new-calculation__block" + (props.new_calculation_modal.block.appear !== true ? " new-calculation__block_hidden" : "") + (props.new_calculation_modal.block.show !== true ? " new-calculation__block_none" : "")}>
                 <button onClick={add_calculation} className="new-calculation__add">{text_content["add_" + props.lang]}</button>
             </div>
         </div>
