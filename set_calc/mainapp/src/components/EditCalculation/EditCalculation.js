93e5361dd4c5e8afdbcbf433cc2b487f97684ccc
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-03-09 04:57:22 +0500

    - add input manager

--- set_calc/mainapp/src/components/EditCalculation/EditCalculation.js
+++ set_calc/mainapp/src/components/EditCalculation/EditCalculation.js
@@ -17,14 +17,30 @@ export default function EditCalculation(props) {
         props.close_calculation_modal(props.set_edit_calculation_modal)
     }
     return (
-        <div onClick={() => {props.close_calculation_modal(props.set_edit_calculation_modal)}} className={props.edit_calculation_modal.wrapper.appear ? "editcalculation" : "editcalculation editcalculation_hidden"}>
-            <div onClick={(e) => {e.stopPropagation()}} className={"editcalculation__block" + (props.edit_calculation_modal.block.appear !== true ? " editcalculation__block_hidden" : "") + (props.edit_calculation_modal.block.show !== true ? " editcalculation__block_none" : "")}>
+        <div onClick={() => {
+            props.close_calculation_modal(props.set_edit_calculation_modal)
+        }}
+             className={props.edit_calculation_modal.wrapper.appear ? "editcalculation" : "editcalculation editcalculation_hidden"}>
+            <div onClick={(e) => {
+                e.stopPropagation()
+            }}
+                 className={"editcalculation__block" + (props.edit_calculation_modal.block.appear !== true ? " editcalculation__block_hidden" : "") + (props.edit_calculation_modal.block.show !== true ? " editcalculation__block_none" : "")}>
                 <p className="editcalculation__title">{text_content['title_' + props.lang]}</p>
+                <input type="text" list="mans"/>
+                <datalist id="mans">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </datalist>
                 <div className="editcalculation__button-wrapper">
-                    <button onClick={edit_calculation} className="editcalculation__btn editcalculation__btn_yellow">{text_content["edit_" + props.lang]}</button>
-                    <button onClick={() => {props.close_calculation_modal(props.set_edit_calculation_modal)}} className="editcalculation__btn editcalculation__btn_black">{text_content["cancel_" + props.lang]}</button>
+                    <button onClick={edit_calculation}
+                            className="editcalculation__btn editcalculation__btn_yellow">{text_content["edit_" + props.lang]}</button>
+                    <button onClick={() => {
+                        props.close_calculation_modal(props.set_edit_calculation_modal)
+                    }}
+                            className="editcalculation__btn editcalculation__btn_black">{text_content["cancel_" + props.lang]}</button>
                 </div>
             </div>
         </div>
-    )
+)
 }
