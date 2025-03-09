96359c096e39ef0a61dcb626cfc5394edd04c888
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-03-10 01:34:49 +0500

    - add blind search to manager input

--- set_calc/mainapp/src/components/EditCalculation/EditCalculation.js
+++ set_calc/mainapp/src/components/EditCalculation/EditCalculation.js
@@ -1,9 +1,11 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "./EditCalculation.css";
 import api_sender from "../api_sender";
 
 
 export default function EditCalculation(props) {
+    const [calculation_manager, set_calculation_manager] = useState(props.calculation.user.username)
+    const [managers, set_managers] = useState([props.calculation.user])
     const text_content = {
         "title_ru": "Изменение",
         "title_uz": "Haqiqatan ham hisob-kitobni o‘chirib tashlamoqchimisiz?",
@@ -16,6 +18,20 @@ export default function EditCalculation(props) {
         props.set_calculations_update(true)
         props.close_calculation_modal(props.set_edit_calculation_modal)
     }
+    const calculation_manager_change = (text) => {
+        const r = api_sender('UserGet', 1, {username: text})
+        r.then(data => {
+            set_managers(data.result)
+            set_calculation_manager(text)
+        })
+    }
+    const calculation_manager_blur = (text) => {
+        let u = managers.find(user => user.username === text)
+        console.log(u)
+        if (u === undefined) {
+            set_calculation_manager("")
+        }
+    }
     return (
         <div onClick={() => {
             props.close_calculation_modal(props.set_edit_calculation_modal)
@@ -26,11 +42,11 @@ export default function EditCalculation(props) {
             }}
                  className={"editcalculation__block" + (props.edit_calculation_modal.block.appear !== true ? " editcalculation__block_hidden" : "") + (props.edit_calculation_modal.block.show !== true ? " editcalculation__block_none" : "")}>
                 <p className="editcalculation__title">{text_content['title_' + props.lang]}</p>
-                <input type="text" list="mans"/>
-                <datalist id="mans">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
+                <input onBlur={(e) => {calculation_manager_blur(e.target.value)}} list="managers" onChange={(e) => {calculation_manager_change(e.target.value)}} value={calculation_manager} className="editcalculation__input" type="text"/>
+                <datalist id="managers">
+                    {managers.map((manager) => (
+                        <option key={manager.id} value={manager.username}></option>
+                    ) )}
                 </datalist>
                 <div className="editcalculation__button-wrapper">
                     <button onClick={edit_calculation}
