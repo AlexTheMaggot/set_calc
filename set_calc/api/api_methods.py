d88f7045c4fca4b402d0d589b719a505a375015a
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-03-10 00:26:49 +0500

    - add auth required decorator and user get api method

--- set_calc/api/api_methods.py
+++ set_calc/api/api_methods.py
@@ -12,6 +12,15 @@ ERRORS = {
 }
 
 
+def auth_required(func):
+    def wrapper(request, request_data):
+        if request.user.is_authenticated:
+            return func(request, request_data)
+        else:
+            return make_error(request_data['id'], 1001)
+    return wrapper
+
+
 def make_success(api_id, result: Any = 'Success'):
     response = {
         'jsonrpc': '2.0',
@@ -46,15 +55,18 @@ def auth(request, request_data):
         return make_error(request_data['id'], 1001)
 
 
+@auth_required
 def lang_check(request, request_data):
     return make_success(request_data['id'], request.user.userprofile.lang)
 
 
+@auth_required
 def logout(request, request_data):
     logout_view(request)
     return make_success(request_data['id'])
 
 
+@auth_required
 def lang_change(request, request_data):
     user = request.user
     user.userprofile.lang = request_data['params']['lang']
@@ -62,6 +74,7 @@ def lang_change(request, request_data):
     return make_success(request_data['id'])
 
 
+@auth_required
 def profile_get(request, request_data):
     user = request.user
     data = {
@@ -70,6 +83,7 @@ def profile_get(request, request_data):
     return make_success(request_data['id'], data)
 
 
+@auth_required
 def calculation_add(request, request_data):
     user = request.user
     calculation = Calculation.objects.create(user=user)
@@ -77,6 +91,7 @@ def calculation_add(request, request_data):
     return make_success(request_data['id'])
 
 
+@auth_required
 def calculation_get_list(request, request_data):
     calculations = Calculation.objects.all()
     result = []
@@ -92,7 +107,20 @@ def calculation_get_list(request, request_data):
     return make_success(request_data['id'], result)
 
 
+@auth_required
 def calculation_delete(request, request_data):
     calculation = Calculation.objects.get(id=request_data['params']['id'])
     calculation.delete()
     return make_success(request_data['id'])
+
+
+@auth_required
+def user_get(request, request_data):
+    users = User.objects.all()
+    result = []
+    for user in users:
+        result.append({
+            'id': user.id,
+            'username': user.username,
+        })
+    return make_success(request_data['id'], result)
