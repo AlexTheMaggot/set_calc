a7998fc8a4437795fe9353373b1bb5443a9fd9cf
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-03-30 02:51:53 +0500

    - fix modal function

--- set_calc/mainapp/src/components/NewCalculation/NewCalculation.js
+++ set_calc/mainapp/src/components/NewCalculation/NewCalculation.js
@@ -14,15 +14,15 @@ export default function NewCalculation(props) {
         let r = api_sender('CalculationAdd', 1)
         r.then(data => {
             props.set_calculations_update(true)
-            props.close_calculation_modal(props.set_new_calculation_modal)
+            props.close_modal(props.set_new_calculation_modal)
         })
     }
     return (
-        <div onClick={() => {props.close_calculation_modal(props.set_new_calculation_modal)}} className={props.new_calculation_modal.wrapper.appear ? "newcalculation" : "newcalculation newcalculation_hidden"}>
+        <div onClick={() => {props.close_modal(props.set_new_calculation_modal)}} className={props.new_calculation_modal.wrapper.appear ? "newcalculation" : "newcalculation newcalculation_hidden"}>
             <div onClick={(e) => {e.stopPropagation()}} className={"newcalculation__block" + (props.new_calculation_modal.block.appear !== true ? " newcalculation__block_hidden" : "") + (props.new_calculation_modal.block.show !== true ? " newcalculation__block_none" : "")}>
                 <div className="newcalculation__btn-wrapper">
                     <button onClick={add_calculation} className="newcalculation__btn newcalculation__btn_green">{text_content["add_" + props.lang]}</button>
-                    <button onClick={() => {props.close_calculation_modal(props.set_new_calculation_modal)}} className="newcalculation__btn newcalculation__btn_black">{text_content[`cancel_${props.lang}`]}</button>
+                    <button onClick={() => {props.close_modal(props.set_new_calculation_modal)}} className="newcalculation__btn newcalculation__btn_black">{text_content[`cancel_${props.lang}`]}</button>
                 </div>
             </div>
         </div>
